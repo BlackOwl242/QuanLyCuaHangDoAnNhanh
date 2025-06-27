db7c5673316d5e6fa9b2ecc8e34e68c209bbf439
BlackOwl242 <141981096+BlackOwl242@users.noreply.github.com>
2025-06-27 16:56:17 +0700

    feat: Update Chart

--- UserControls/ucRevenue.Designer.cs
+++ UserControls/ucRevenue.Designer.cs
@@ -32,9 +32,6 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnView = new System.Windows.Forms.Button();
             this.pnlTop = new System.Windows.Forms.Panel();
             this.dtpCheckIn = new System.Windows.Forms.DateTimePicker();
@@ -168,6 +165,8 @@
             // 
             // chart2
             // 
+            this.chart2.BackColor = System.Drawing.Color.Transparent;
+            this.chart2.BorderlineColor = System.Drawing.Color.Transparent;
             chartArea1.Name = "ChartArea1";
             this.chart2.ChartAreas.Add(chartArea1);
             this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
